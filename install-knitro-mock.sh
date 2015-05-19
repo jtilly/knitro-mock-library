8aa869f1cae2e0ce8f1a271566fb8d448458beea
Jan Tilly <jantilly@gmail.com>
2015-05-19 14:31:42 -0400

    Update install-knitro-mock.sh

--- install-knitro-mock.sh
+++ install-knitro-mock.sh
@@ -3,11 +3,15 @@
 # create folder structure and download skeleton
 mkdir ~/knitro-mock-library
 cd ~/knitro-mock-library
-wget https://github.com/jtilly/knitro-mock-library/raw/master/skeleton.zip
-unzip skeleton.zip
+mkdir include
+cd include
+wget https://github.com/jtilly/knitro-mock-library/raw/master/include/knitro.h
+cd ../
+mkdir lib
 cd lib
-# compile knitro-mock-library.cpp
-gcc -fPIC -c knitro-mock-library.cpp
+wget https://github.com/jtilly/knitro-mock-library/raw/master/lib/knitro-mock-library.c
+# compile knitro-mock-library.c
+gcc -fPIC -c knitro-mock-library.c
 # create the so
 gcc -shared -Wl,-soname,libknitro.so.9 -o libknitro.so.9.1 knitro-mock-library.o
 ln -sf libknitro.so.9.1 libknitro.so
