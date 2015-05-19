6ee93c8db67a6d1d2d78e0f68d82e87926d8111c
Jan Tilly <jantilly@gmail.com>
2015-05-19 11:46:40 -0400

    renamed file

--- install-knitro-mock.sh
+++ install-knitro-mock.sh
@@ -12,6 +12,4 @@ gcc -fPIC -c knitro-mock-library.cpp
 gcc -shared -Wl,-soname,libknitro.so.9 -o libknitro.so.9.1 knitro-mock-library.o
 ln -sf libknitro.so.9.1 libknitro.so
 ln -sf libknitro.so.9.1 libknitro.so.9
-
-export KNITRO=~/knitro-mock-library
-export LD_LIBRARY_PATH=~/knitro-mock-library/lib
+cd ~
