655d08d598142d3b526518d80ebca25821bdefad
Jan Tilly <jantilly@gmail.com>
2015-05-19 14:34:05 -0400

    Update install-knitro-mock.sh

--- install-knitro-mock.sh
+++ install-knitro-mock.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+# this script is used by travis to build knitroR
+
 # create folder structure and download skeleton
 mkdir ~/knitro-mock-library
 cd ~/knitro-mock-library
@@ -10,8 +12,10 @@ cd ../
 mkdir lib
 cd lib
 wget https://github.com/jtilly/knitro-mock-library/raw/master/lib/knitro-mock-library.c
+
 # compile knitro-mock-library.c
 gcc -fPIC -c knitro-mock-library.c
+
 # create the so
 gcc -shared -Wl,-soname,libknitro.so.9 -o libknitro.so.9.1 knitro-mock-library.o
 ln -sf libknitro.so.9.1 libknitro.so
